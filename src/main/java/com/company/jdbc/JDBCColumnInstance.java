8fae9c954512b72e04ef7062b217ebd2efa34756
RashadHamidli <mr_rashad@email.com>
2023-03-21 07:48:47 +0400

    Merge remote-tracking branch 'origin/main'

--- src/main/java/com/company/jdbc/JDBCColumnInstance.java
+++ src/main/java/com/company/jdbc/JDBCColumnInstance.java
@@ -3,7 +3,6 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-
 /**
  *
  * @author mr_rashad
